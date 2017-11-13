d32b70e0153546b121fd5dd47ad3ecb315830115
Eric Rogers <eric.rog17@gmail.com>
2017-11-13 06:02:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ function takeANumber(katzDeliLine, customerName) {
   // return a message about their position in line
   return 'Welcome, ' + customerName + '. You are number ' + katzDeliLine.length + ' in line.'
 }
+
+function nowServing() {
+  // first check to see if there are customers waiting to be served
+  if(katzDeliLine === []) {
+    return 'There is nobody waiting to be served!'
+  } else {
+    // grab the person being served before updating array
+    let nowServed = katzDeliLine[0];
+    // update the array by removing the first person
+    katzDeliLine.shift();
+    return 'currently serving ' + nowServed + '.';
+  }
+}
