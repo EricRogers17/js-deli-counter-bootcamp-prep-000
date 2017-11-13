733b28196409fc5ddf12f5683b33c417c3a00489
Eric Rogers <eric.rog17@gmail.com>
2017-11-13 05:45:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, customerName) {
-  let updateLine = [];
-  return updateLine.push(customerName);
+  // update the line by pushing the new customer onto the array
+  katzDeliLine.push(customerName);
+  // return a message about their position in line
+  return 'Welcome, ' + customerName + '. You are number ' + katzDeliLine.length + ' in line.'
 }
